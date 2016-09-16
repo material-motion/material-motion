1d87f646a371d0ffa78756431011ea2fafcade46
Jeff Verkoeyen <featherless@google.com>
2016-09-16 16:56:22 -0400

    Add mdm sprint.
    Summary:
    Includes:
    
    - mdm sprint current
    - mdm sprint open
    
    Reviewers: markwei, O2 Material Motion
    
    Reviewed By: markwei, O2 Material Motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D1623

---
+++ contributor_tools/sprint/README.md
@@ -0,0 +1,7 @@
+Usage: `mdm sprint current`
+
+    Output the path to the current sprint, if one exists.
+
+Usage: `mdm sprint open`
+
+    Open the current sprint in a web browser, if one exists.
---
+++ contributor_tools/sprint/get_current_sprint.php
@@ -0,0 +1,28 @@
+<?php
+
+$repo = $argv[1];
+$token = $argv[2];
+
+include 'common.php';
+
+// MAIN script logic
+
+// Get teams
+$ch = createCurlRequest("repos/$repo/projects");
+$server_output = curl_exec($ch);
+curl_close($ch);
+if (!checkCurlResponse($server_output)) {
+  return;
+}
+
+$projects = json_decode($server_output, TRUE);
+
+foreach ($projects as $project) {
+  if ($project['name'] == 'Current sprint') {
+    echo str_replace("https://api.github.com/repos/", "", $project['url'])."\n";
+    exit(0);
+  }
+}
+
+exit(1);
+
