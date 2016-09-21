edd946ab8c4c0f224890fae836511a2671d45f11
Jeff Verkoeyen <featherless@google.com>
2016-09-21 12:44:43 -0400

    mdm sprint start now sets the sprint start date in the description.

--- contributor_tools/sprint/start_sprint.php
+++ contributor_tools/sprint/start_sprint.php
@@ -10,7 +10,7 @@ include 'common.php';
 echo "Creating sprint...\n";
 $ch = createDataRequest("repos/$repo/projects", array(
   'name' => "Current sprint",
-  'body' => "Sprint end date: ".date('Y-n-d', strtotime('next Wednesday'))
+  'body' => "Sprint start date: ".date('Y-n-d')
 ));
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 $server_output = curl_exec($ch);
